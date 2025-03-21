910264dd6cdf527b3549ca130acc988b01e72be6
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-03-21 12:37:53 -0300

    Atualizando calculadora

--- aula-20-03-forms-js/js/calculadora.js
+++ aula-20-03-forms-js/js/calculadora.js
@@ -1,19 +1,37 @@
-const btnSomar = document.getElementById('btnSomar');
+const form = document.getElementById('formCalc');
+const btnMais = document.getElementById('btnMais');
 const btnMenos = document.getElementById('btnMenos');
 const btnMult = document.getElementById('btnMult');
 const btnDiv = document.getElementById('btnDiv');
+const btnResult = document.getElementById('btnResult');
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
+const result = document.getElementById('resultado');
 
-btnSomar.addEventListener('click', function(event) {
+/** Pegando o valor dos input e colocando numa váriavel valor */
+const valor1 = Number(input1.value); 
+const valor2 = Number(input2.value);
 
-    /** 
-     * Armazenando o valor do 
-     * input na variável 
-     * */
-    const valor1 = input1.value; 
-    const valor2 = input2.value;
+/** Variável do valor total */
+let total;
 
-    window.alert(valor1);
+
+/** Botão de Soma */
+btnMais.addEventListener('click', function(event) {
+
+    total = valor1 + valor2;
+
+    result.innerHTML = total;
 
 });
+
+/** Botão de Subtração */
+
+btnMenos.addEventListener('click', function(){
+
+    //total = 
+
+});
+/** Botão de Multiplicação */
+/** Botão de Divisão */
+/** Botão de Resultado */
