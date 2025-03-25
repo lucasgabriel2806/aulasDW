98b208f4a471acda871402e7d8d0ffa725be1215
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-03-25 18:12:10 -0300

    Adicionando mais botões

--- aula-20-03-forms-js/js/calculadora.js
+++ aula-20-03-forms-js/js/calculadora.js
@@ -1,4 +1,3 @@
-const form = document.getElementById('formCalc');
 const btnMais = document.getElementById('btnMais');
 const btnMenos = document.getElementById('btnMenos');
 const btnMult = document.getElementById('btnMult');
@@ -8,6 +7,8 @@ const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
 const result = document.getElementById('resultado');
 
+let sinal;
+
 /** Pegando o valor dos input e colocando numa váriavel valor */
 const valor1 = Number(input1.value); 
 const valor2 = Number(input2.value);
@@ -16,40 +17,49 @@ const valor2 = Number(input2.value);
 let total;
 
 
+
+
 /** Botão de Soma */
 btnMais.addEventListener('click', function() {
 
-    total = valor1 + valor2;
-
-    result.innerHTML = total;
+    sinal = "+";
 
 });
 
 /** Botão de Subtração */
 btnMenos.addEventListener('click', function(){
 
-    total = valor1 - valor2;
-
-    result.innerHTML = total; 
+    sinal = "-";
 
 });
 
 /** Botão de Multiplicação */
 btnMult.addEventListener('click', function() {
 
-    total = valor1 * valor2;
-
-    result.innerHTML = total;
+    sinal = "*";
 
 });
 
 /** Botão de Divisão */
 btnDiv.addEventListener('click', function() {
 
-    total = valor1 / valor2;
-
-    result.innerHTML = total;
+    sinal = "/";
 
 });
 
 /** Botão de Resultado */
+btnResult.addEventListener('click', function() {
+
+    if (sinal == "+") {
+        total = valor1 + valor2;
+    } else if (sinal == "-") {
+        total = valor1 - valor2;
+    } else if (sinal == "*") {
+        total = valor1 * valor2;
+    } else if (sinal == "/") {
+        total = valor1 / valor2;
+    }
+
+    result.innerHTML = total;
+
+})
