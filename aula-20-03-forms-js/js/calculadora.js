ceaa388f3bb37b76dbdc479b3ba6ab805a616fe0
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-03-26 04:38:54 -0300

    Adicionando interatividade aos botoes de número

--- aula-20-03-forms-js/js/calculadora.js
+++ aula-20-03-forms-js/js/calculadora.js
@@ -7,6 +7,17 @@ const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
 const result = document.getElementById('resultado');
 
+const num0 = document.getElementById('num0');
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const num3 = document.getElementById('num3');
+const num4 = document.getElementById('num4');
+const num5 = document.getElementById('num5');
+const num6 = document.getElementById('num6');
+const num7 = document.getElementById('num7');
+const num8 = document.getElementById('num8');
+const num9 = document.getElementById('num9');
+
 let sinal;
 
 /** Pegando o valor dos input e colocando numa váriavel valor */
@@ -16,7 +27,11 @@ const valor2 = Number(input2.value);
 /** Variável do valor total */
 let total;
 
+num1.addEventListener('click', function() {
 
+    result.innerHTML += '1';1
+
+})
 
 
 /** Botão de Soma */
