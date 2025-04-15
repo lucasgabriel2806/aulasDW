0f314292122dd16d1c5fe2df725b694364e552b9
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-04-15 10:43:30 -0300

    Adicionando botão de apagar um número

--- aula-27-03-revisao/js/script.js
+++ aula-27-03-revisao/js/script.js
@@ -1,4 +1,4 @@
-/** Calculadora com INPUTs */
+/** Calculadora 2 - com INPUTs */
 var num1 = document.getElementById('num1');
 var num2 = document.getElementById('num2');
 var resultado2 = document.getElementById('resultado2');
@@ -25,17 +25,25 @@ btnMult.addEventListener('click', function() {
 });
 
 // ------------------------------------------------------------ 
+// Calculadora 1 
+// ------------------------------------------------------------ 
 
+// calculo é o 1º display
 var calculo = document.getElementById('calculo');
+// calculo é o 2º display
 var resultado = document.getElementById('resultado');
+// limpa tudo
 var limpar = document.getElementById('limpar');
+// limpa o último número
 var limpar1 = document.getElementById('limpar1');
+// operadores
 var soma = document.getElementById('soma');
 var subtracao = document.getElementById('subtracao');
 var multiplicacao = document.getElementById('multiplicacao');
 var divisao = document.getElementById('divisao');
-var ponto = document.getElementById('ponto');
 var igual = document.getElementById('igual');
+// numeros e ponto
+var ponto = document.getElementById('ponto');
 var n9 = document.getElementById('n9');
 var n8 = document.getElementById('n8');
 var n7 = document.getElementById('n7');
@@ -47,20 +55,29 @@ var n2 = document.getElementById('n2');
 var n1 = document.getElementById('n1');
 var n0 = document.getElementById('n0');
 
+// 1º e 2º número que digito
 var numero1 = '';
 var numero2 = '';
+// vai armazenar a operação atual
 var operacao = '';
 var ponto_valor = '';
+// verifica se tem ponto no 1º e 2º número
 var tem_ponto1 = false;
 var tem_ponto2 = false;
+
+
 
 
 /** ---------------- */
 /** Números */
 /** ---------------- */
 document.addEventListener('click', function (event) {
-    console.log(event.target.classList);
+    // event é um objeto que contém informações sobre o evento que aconteceu.
+    // event.target faz referência ao elemento HTML no qual o evento foi disparado.
+    // classList pega as classes
+    // console.log(event.target.classList);
     
+    // Se o elemento clicado 
     if (event.target.classList.contains('numeros')) {
         if(operacao == '') {
             numero1 = numero1 + event.target.textContent;
@@ -70,8 +87,6 @@ document.addEventListener('click', function (event) {
             calculo.innerText = numero1 + " " + operacao + " " + numero2;
         }
     }
-
-    //console.log('calculo: ' + calculo.innerText.split(''));
 });
 
 
@@ -201,8 +216,20 @@ limpar.addEventListener('click', function() {
     tem_ponto2 = false;
 });
 
+const listaCalculo = calculo.innerText.split('');
+const listaNumero1 = numero1.innerText.split('');
+
 limpar1.addEventListener('click', function() {
-    // const listaCalculo = calculo.innerText.split('');
+    // console.log('calculo: ' + calculo.innerText.split('').pop());
+    
+    console.log(listaCalculo);
+    listaCalculo.pop();
+    console.log(listaCalculo);
+
+    console.log(listaNumero1);
+    listaNumero1.pop();
+    console.log(listaNumero1);
+    // alert("oi");
 });
 
 
